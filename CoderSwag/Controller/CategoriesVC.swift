f0ab6818296573a24f7adbd05be9bd084f8a7158
Matt Tripodi <mtripodi1027@gmail.com>
2017-08-02 18:50:07 -0400

    Created category screen UI

---
+++ CoderSwag/Controller/CategoriesVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CoderSwag
+//
+//  Created by Matt Tripodi on 8/2/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+	
+	@IBOutlet weak var categoryTable: UITableView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+
+}
+
